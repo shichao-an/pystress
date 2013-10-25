1680e33ada2cd81736b0e166aee78044ea172533
Shichao An <moleculeandrew@gmail.com>
2013-10-24 23:42:10 -0400

    update

--- stress.py
+++ stress.py
@@ -1,6 +1,27 @@
+from multiprocessing import Process
+import os
+import sys
+import time
+
+
 def stress():
+    a = 100.01
+    b = 99.99
+    c = None
+    print 'process id:', os.getpid()
     while True:
-        a = 1
+        c = a * b
+    return c
+
 
 if __name__ == "__main__":
-    stress()
+    procs = []
+    for i in range(5):
+        p = Process(target=stress)
+        p.start()
+        procs.append(p)
+
+    time.sleep(int(sys.argv[1]))
+    
+    for p in procs:
+        p.terminate()
